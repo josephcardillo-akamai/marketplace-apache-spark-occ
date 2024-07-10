183ead078e7b63b3aac45bb593b37180aae1e869
Joseph Cardillo <joseph.s.cardillo@gmail.com>
2024-07-10 15:30:31 -0400

    update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,10 +6,10 @@ if [ "${DEBUG}" == "NO" ]; then
 fi
 
 # constants
-readonly ROOT_PASS=$(sudo cat /etc/shadow | grep root)
-readonly LINODE_PARAMS=($(curl -sH "Authorization: Bearer ${TOKEN_PASSWORD}" "https://api.linode.com/v4/linode/instances/${LINODE_ID}" | jq -r .type,.region,.image))
-readonly LINODE_TAGS=$(curl -sH "Authorization: Bearer ${TOKEN_PASSWORD}" "https://api.linode.com/v4/linode/instances/${LINODE_ID}" | jq -r .tags)
-readonly VARS_PATH="./group_vars/spark/vars"
+# readonly ROOT_PASS=$(sudo cat /etc/shadow | grep root)
+# readonly LINODE_PARAMS=($(curl -sH "Authorization: Bearer ${TOKEN_PASSWORD}" "https://api.linode.com/v4/linode/instances/${LINODE_ID}" | jq -r .type,.region,.image))
+# readonly LINODE_TAGS=$(curl -sH "Authorization: Bearer ${TOKEN_PASSWORD}" "https://api.linode.com/v4/linode/instances/${LINODE_ID}" | jq -r .tags)
+# readonly VARS_PATH="./group_vars/spark/vars"
 
 # utility functions
 function destroy {
@@ -45,35 +45,36 @@ function master_ssh_key {
 
 # production
 function build {
-  # secrets
+  local LINODE_PARAMS=($(curl -sH "Authorization: Bearer ${TOKEN_PASSWORD}" "https://api.linode.com/v4/linode/instances/${LINODE_ID}" | jq -r .type,.region,.image))
+  local LINODE_TAGS=$(curl -sH "Authorization: Bearer ${TOKEN_PASSWORD}" "https://api.linode.com/v4/linode/instances/${LINODE_ID}" | jq -r .tags)
+  local VARS_PATH="${WORK_DIR}/group_vars/spark/vars"
+  local TEMP_ROOT_PASS=$(openssl rand -base64 32)
   master_ssh_key
 
   # write vars file
   sed 's/  //g' <<EOF > ${VARS_PATH}
+  # user vars
+  sudo_username: ${SUDO_USERNAME}
+  token_password: ${TOKEN_PASSWORD}
   
   # deployment vars
   uuid: ${UUID}
   ssh_keys: ${ANSIBLE_SSH_PUB_KEY}
   instance_prefix: ${INSTANCE_PREFIX}
-  cluster_name: ${CLUSTER_NAME}
   type: ${LINODE_PARAMS[0]}
   region: ${LINODE_PARAMS[1]}
   image: ${LINODE_PARAMS[2]}
-  
   linode_tags: ${LINODE_TAGS}
+  root_pass: ${TEMP_ROOT_PASS}
   
-  # sudo user
-  sudo_username: ${SUDO_USERNAME}
-
-  soa_email_address: ${SOA_EMAIL_ADDRESS}
-  spark_user: ${SPARK_USER}
+  # spark vars
+  cluster_name: ${CLUSTER_NAME}
   cluster_size: ${CLUSTER_SIZE}
-EOF
-  if [[ -n ${TOKEN_PASSWORD} ]]; then
-    echo "token_password: ${TOKEN_PASSWORD}" >> ${VARS_PATH};
-  else echo "No API token entered";
-  fi
+  spark_user: ${SPARK_USER}
 
+  # ssl/tls
+  soa_email_address: ${SOA_EMAIL_ADDRESS}
+EOF
   if [[ -n ${DOMAIN} ]]; then
     echo "domain: ${DOMAIN}" >> ${VARS_PATH};
   else echo "default_dns: $(hostname -I | awk '{print $1}'| tr '.' '-' | awk {'print $1 ".ip.linodeusercontent.com"'})" >> ${VARS_PATH};
@@ -85,10 +86,14 @@ EOF
   fi
 }
 
-function deploy {
-  ansible-playbook provision.yml
-  ansible-playbook -v -i hosts site.yml --extra-vars "root_pass=${ROOT_PASS}"
+function deploy { 
+    for playbook in provision.yml site.yml; do ansible-playbook -v -i hosts $playbook; done
 }
+
+# function deploy {
+#   ansible-playbook provision.yml
+#   ansible-playbook -v -i hosts site.yml --extra-vars "root_pass=${ROOT_PASS}"
+# }
 
 ## cleanup ##
 function cleanup {
